d5f730b321f27c2f9b48ca1335939b61f215ae80
Camille Bernard <camille.bernard@edics.fr>
2020-03-13 11:58:24 +0100

    First test finished

--- test/ReaderTest.php
+++ test/ReaderTest.php
@@ -2,9 +2,9 @@
 
 class ReaderTest extends \PHPUnit\Framework\TestCase
 {
-    public function testReadCSV()
+    public function testReadCsv()
     {
-        $reader = new \Lianhua\SuperCSV\Reader(__DIR__ . DIRECTORY_SEPARATOR . "test" . DIRECTORY_SEPARATOR . "01.csv");
+        $reader = new \Lianhua\SuperCSV\Reader(__DIR__ . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "01.csv");
 
         //Line 1
         $line = $reader->read();
@@ -18,9 +18,9 @@ class ReaderTest extends \PHPUnit\Framework\TestCase
         $line = $reader->read();
         $this->assertIsArray($line);
         $this->assertEquals("G", $line[0]);
-        $this->assertEquals("H", $line[0]);
-        $this->assertEquals("I", $line[0]);
-        $this->assertEquals("J", $line[0]);
+        $this->assertEquals("H,I", $line[1]);
+        $this->assertEquals("J", $line[2]);
+        $this->assertEquals("K", $line[3]);
 
         //End of file
         $line = $reader->read();
